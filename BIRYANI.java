47c9879065cf65c55e0656c53a5a1caea63ecdfd
Marut Lashkari <98412051+marutlashkari@users.noreply.github.com>
2023-01-06 21:44:40 +0530

    Add files via upload

---
+++ BIRYANI.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		// x is week
+		// y is cost of class per week
+		// find total amount
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for(int i = 1; i <= t; i++){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    System.out.println(x*y);
+		}
+	}
+}
