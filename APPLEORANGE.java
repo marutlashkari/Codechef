d90497b05212739733b2977b28ee07969f514be3
Marut Lashkari <98412051+marutlashkari@users.noreply.github.com>
2023-01-11 20:55:38 +0530

    Add files via upload

---
+++ APPLEORANGE.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0) {
+		    int n = sc.nextInt();
+		    int m = sc.nextInt();
+		    
+		    int ans = hcf(n,m);
+            System.out.println(ans);
+		}
+	}
+	public static int hcf(int n1, int n2)
+    {
+        if (n2 != 0)
+            return hcf(n2, n1 % n2);
+        else
+            return n1;
+    }
+}
